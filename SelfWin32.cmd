38b630e2d4126b3708e82f0f2817712493376ece
AntKrotov <akron1_m@mail.ru>
2023-02-04 19:33:12 +0300

    добавлен параметр -fa <size> (выравнивание секций PE)

--- SelfWin32.cmd
+++ SelfWin32.cmd
@@ -1,2 +1,2 @@
-Compiler.exe source\Compiler.ob07 win32con -out bin\Compiler32.exe -stk 2
+Compiler.exe source\Compiler.ob07 win32con -out bin\Compiler32.exe -stk 2 -fa 512
 @pause
--- SelfWin64.cmd
+++ SelfWin64.cmd
@@ -1,2 +1,2 @@
-Compiler.exe source\Compiler.ob07 win64con -out bin\Compiler.exe -stk 2
+Compiler.exe source\Compiler.ob07 win64con -out bin\Compiler.exe -stk 2 -fa 512
 @pause
